6b9988724e54368566f9ce29e1b1370cedb6339d
Elijah White <elijah.white1313@gmail.com>
2025-07-29 21:52:37 -0500

    Made the Pngs into UI/Sprites. Made TowerPlace Manager working, but still getting compiling error.

--- Assets/Scripts/Managers/TowerPlaceManager.cs
+++ Assets/Scripts/Managers/TowerPlaceManager.cs
@@ -6,6 +6,14 @@ public class TowerPlaceManager : MonoBehaviour
     public Camera MainCamera;
     public LayerMask TileLayer;
     public InputAction PlaceTowerAction;
+
+    [SerializeField] private float towerPlacementHeightOffset = .2f;
+    private GameObject currentTowerToSpawn;
+    private GameObject towerPreview;
+    private Vector3 towerPlacementPosition;
+
+    [SerializeField] private bool isPlacingTower = false;
+    private bool isTileSelected = false;
     void Start()
     {
         
@@ -14,7 +22,22 @@ public class TowerPlaceManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlacingTower)
+        {
+            Ray ray = MainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if(Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity, TileLayer))
+            {
+                towerPlacementPosition = hitInfo.transform.position + Vector3.up * towerPlacementHeightOffset;
+                towerPreview.transform.position = towerPlacementPosition;
+                towerPreview.SetActive(true);
+                isTileSelected = true;
+            }
+            else
+            {
+                towerPreview.SetActive(false);
+                isTileSelected=false;
+            }
+        }
     }
 
     private void OnEnable()
@@ -28,9 +51,29 @@ public class TowerPlaceManager : MonoBehaviour
         PlaceTowerAction.Disable();
         PlaceTowerAction.performed -= OnPlaceTower;
     }
-
+    public void StartPlacingTower(GameObject towerPrefab)
+    {
+        if (currentTowerToSpawn != towerPrefab)
+        {
+            isPlacingTower = true;
+            currentTowerToSpawn = towerPrefab;
+            towerPreview = Instantiate(currentTowerToSpawn);
+            if(towerPreview != null)
+            {
+                Destroy(towerPreview);
+            }
+        }
+        
+    }
     private void OnPlaceTower(InputAction.CallbackContext context)
     {
-
+        if (isPlacingTower && isTileSelected)
+        {
+            isPlacingTower = false;
+            Instantiate(currentTowerToSpawn, towerPlacementPosition, Quaternion.identity);
+            Destroy(towerPreview);
+            currentTowerToSpawn = null;
+        }
+        
     }
 }
