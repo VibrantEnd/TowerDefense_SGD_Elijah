40aaec7e6f225bfb5f98c8262edfd1ddbc5780d4
Elijah White <elijah.white1313@gmail.com>
2025-07-29 19:20:46 -0500

    Introduced structure and renamed enemy.

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -1,17 +1,41 @@
+using System.Collections;
 using UnityEngine;
+
+public struct SpawnData
+{
+    public GameObject EnemyToSpawn;
+    public float TimeBeforeSpawn;
+    public Transform SpawnPoint;
+    public Transform EndPoint;
+}
 
 public class WaveManager : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] private Transform endPoint;
+    [SerializeField] private float timeBetweenSpawns = 5f;
+    [SerializeField] private float timeBetweenWaves = 5f;
+
+    [SerializeField] private int enemyCount = 5;
+    [SerializeField] private int waveCount = 2;
     void Start()
     {
-        SpawnEnemy();
+        StartCoroutine(SpawnEnemies());
     }
 
+    IEnumerator SpawnEnemies()
+    {
+       for (int j = 0; j < enemyCount; j++)
+       {
+                yield return new WaitForSeconds(timeBetweenSpawns);
+           SpawnEnemy();
+       }
+    }
     public void SpawnEnemy()
     {
         GameObject enemyInstance = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
         Enemy enemy = enemyInstance.GetComponent<Enemy>();
+        enemy.Initialize(endPoint);
     }
 }
